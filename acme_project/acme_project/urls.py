0c1b554c4e2038fae942eae01f9a8e2d204a3e22
Михаил Киржаков <kirzhakovma@mail.ru>
2024-04-19 14:31:12 +0300

    end of 9 sprint

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,12 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     import debug_toolbar
     # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from .validators import real_age
 
@@ -9,4 +10,16 @@ class Birthday(models.Model):
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
     birthday = models.DateField('Дата рождения', validators=(real_age,))
-    
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+    class Meta():
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
